61a8fded28826168c55418aafb346bfeb0bc9809
Alif Munim <alif.munim@ryerson.ca>
2020-03-25 10:46:08 -0400

    Allow CORS

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv/config");
 
 // Middleware
 // Can be used for authentication
+app.use(cors());
 app.use(bodyParser.json());
 
 // Import Routes
